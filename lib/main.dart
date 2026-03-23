5cfe40dbcbb59754726787f7f7b95d543cc1c21a
evvad <zhenyadibrova77@gmail.com>
2026-03-23 12:15:58 +0200

    refactor: fix broken imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
-import 'package:zvycha_frontend/constants/app_theme.dart';
-import 'package:zvycha_frontend/router/app_router.dart';
-import 'package:zvycha_frontend/notifiers/auth_notifier.dart';
+import 'package:zvycha_frontend/core/theme/app_theme.dart';
+import 'package:zvycha_frontend/core/navigation/app_router.dart';
+import 'package:zvycha_frontend/features/auth/presentation/providers/auth_notifier.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
