67a45024b3fcb4649f46a442da541189a5ac7770
evvad <zhenyadibrova77@gmail.com>
2026-03-24 13:49:46 +0200

    feat: update color palette with gray scales

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,4 +5,9 @@ class AppColors {
   static const primary = Color(0xFF0F372E);
   static const accentGreen = Color(0xFF7ED957);
   static const accentPurple = Color(0xFFBFA6EA);
+  static const white = Color(0xFFFFFFFF);
+  static const gray100 = Color(0xFFF5F5F5);
+  static const gray300 = Color(0xFFCDCDCD);
+  static const gray500 = Color(0xFF9E9E9E);
+  static const black = Color(0x00000000);
 }
